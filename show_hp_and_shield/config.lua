447db7b278607ca71f28c7a394d27b8d0d741de2
ZwerOxotnik <zweroxotnik@gmail.com>
2019-03-03 16:03:01 +0300

    Fix config.lua

--- show_hp_and_shield/config.lua
+++ show_hp_and_shield/config.lua
@@ -1,5 +1,5 @@
-Copyright (C) 2018-2019 ZwerOxotnik <zweroxotnik@gmail.com>
-Licensed under the EUPL, Version 1.2 only (the "LICENCE");
+-- Copyright (C) 2018-2019 ZwerOxotnik <zweroxotnik@gmail.com>
+-- Licensed under the EUPL, Version 1.2 only (the "LICENCE");
 
 return {
 	TICKS_FOR_VEHICLE = 60 * 20 * 1.5
